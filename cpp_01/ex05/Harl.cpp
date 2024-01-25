fc820f03e9f7f33b901ff5b2d64010e3493b6c15
Hyeongseop Shin <hyeongsh@c2r2s7.42seoul.kr>
2024-01-25 19:18:37 +0900

    edit ex05 - this->

--- cpp_01/ex05/Harl.cpp
+++ cpp_01/ex05/Harl.cpp
@@ -34,7 +34,6 @@ Harl::Harl()
 void	Harl::complain(std::string level)
 {
 	std::string	levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-	void	(Harl::*func[4])();
 
 	for (int i = 0; i < 4; i++)
 	{
